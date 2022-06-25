8a8799f68143b82ce42bdc23cfc5a311b8e705a9
Anton Kutsim <kutsimanton@gmail.com>
2022-06-25 15:44:28 +0300

    p4

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
 import debounce from "lodash.debounce";
+import Notiflix from 'notiflix';
+import { fetchCountries } from "./js/fetchCountries";
 import './css/styles.css';
-import countryCardTpl from './templates/country-card.hbs';
+
+
 
 const DEBOUNCE_DELAY = 300;
+
 const refs = {
     input: document.querySelector('#search-box'),
     countryList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('.country-info')
 }
 
-refs.input.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
+refs.input.addEventListener('input', debounce(event => {
+    event.preventDefault();
+    
+    const countries = refs.input.value;
 
-function onSearchCountry(e) {
-    console.log(e.target.value);
-};
+    fetchCountries(countries).then(data => {
+        if (data.length > 2 && data.length < 10) {
+            refs.countryList.innerHTML = 
+            data.map(country => `<li>
+                <p>
+                    <img src="${country.flags.svg}" alt="${country.name.official}" width="17" height="13"/>
+                    ${country.name.common}
+                </p>
+            </li>`).join('');
+        } else if (data.length < 2) {
+            refs.countryList.innerHTML = ' ';
+            refs.countryInfo.innerHTML =
+                data.map(country => 
+                `<div>
+                    <h3>
+                        <img src="${country.flags.svg}" alt="${country.name.official}" width="17" height="13"/>
+                        ${country.name.official}
+                    </h3>
+                    <p><b>Capital: </b>${country.capital}</p>
+                    <p><b>Population: </b>${country.population}</p>
+                    <p><b>Languages: </b>${country.languages}</p>
+                </div>`
+                ).join('');
+        } else if (data.length > 10) {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        }
+    })
+}, DEBOUNCE_DELAY));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,11 @@
-export const fetchCountries = () => {
-    
+export const fetchCountries = (name) => {
+    return fetch(`https://restcountries.com/v3.1/name/${name}`)
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+        });
 }
+// https://restcountries.com/v3.1/name/${name}
+// https://restcountries.com/v2/{service}?fields=${name},{field},{field}
